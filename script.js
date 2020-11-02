1005095ed614c58abac22e9633af003fee5fee88
agajdosi <andreas.gajdosik@gmail.com>
2020-11-02 12:50:45 +0100

    Hide questions via CSS on start not via JS

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-document.getElementById("q2").style.display = "none"
-document.getElementById("q3").style.display = "none"
-
-
-
 function next(ID) {  
   document.getElementById("q" + ID.toString()).style.display = "none"
   document.getElementById("q" + (ID+1).toString()).style.display = "block"
-
+  
   return false
 }
 
 function calculate() {
-
-  console.log("calculating")
-
   var answers = new Array()
   answers.push(document.forms[0].q.value)
   answers.push(document.forms[1].q.value)
   answers.push(document.forms[2].q.value)
-
-  console.log(answers)
-
   var results = frequent(answers)
-  console.log(results)
-
+  
   if (results[0] == "A") {
     window.location.href = "https://www.youtube.com/embed/sGkh1W5cbH4?autoplay=1";
   }
-
   if (results[0] == "B") {
     window.location.href = "https://www.youtube.com/embed/jkLRith2wcc?autoplay=1";
   }
-
   if (results[0] == "C") {
     window.location.href = "https://www.youtube.com/embed/xNN7iTA57jM?autoplay=1";
   }
-
   if (results[0] == "D") {
     window.location.href = "https://www.youtube.com/embed/L_LUpnjgPso?autoplay=1";
   }
 
   return false
 }
-
 
 function frequent(arr) {
   var mf = 1;
